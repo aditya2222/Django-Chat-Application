34003a64aa1078860933ac9cbd18d8753896fc4d
adityasingh222247@gmail.com <adityasingh222247@gmail.com>
2018-07-10 22:29:26 +0530

    Sending Messages Commit

--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -15,6 +15,11 @@ class ChatConsumer(AsyncConsumer):
                 "type" : "websocket.accept" 
                 
                 })
+            other_user = self.scope['url_route']['kwargs']['username']
+            me = self.scope['user']
+            print(other_user,me)
+            thread_obj = await self.get_thread(me, other_user)
+            print(thread_obj)
             # await asyncio.sleep(10)
             await self.send({
                 "type" : "websocket.send",
@@ -24,8 +29,14 @@ class ChatConsumer(AsyncConsumer):
     async def websocket_receive(self, event):
         # When a message is received from websocket
         print("receive",event)
+
     async def websocket_disconnect(self, event):
         #When the socket disconnects
         print("Disconnected",event)
+    
+    @database_sync_to_async # this is done to prevent memeory leaks , overload the database, open too many connections etc
+    def get_thread(self, user, other_username):
+        return Thread.objects.get_or_new(user, other_username)[0]
+    
 
 
