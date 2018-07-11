170973326acc8d6d89b3e6083e8e73daf9d9c76f
adityasingh222247@gmail.com <adityasingh222247@gmail.com>
2018-07-11 15:58:26 +0530

    Final Commit

--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -12,14 +12,24 @@ from .models import Thread, ChatMessage
 class ChatConsumer(AsyncConsumer):
     async def websocket_connect(self, event):
         print("Connected", event)
+
+        other_user = self.scope['url_route']['kwargs']['username']
+        me = self.scope['user']
+        thread_obj = await self.get_thread(me, other_user)
+        self.thread_obj = thread_obj
+        chat_room = "thread_{}".format(thread_obj.id)
+        self.chat_room = chat_room
+
+        await self.channel_layer.group_add(
+            chat_room,
+            self.channel_name  # default attribute of channels
+        )
+
         await self.send({
             "type": "websocket.accept"
 
         })
-        other_user = self.scope['url_route']['kwargs']['username']
-        me = self.scope['user']
-        print(other_user, me)
-        thread_obj = await self.get_thread(me, other_user)
+
         # await asyncio.sleep(10)
 
     async def websocket_receive(self, event):
@@ -33,14 +43,27 @@ class ChatConsumer(AsyncConsumer):
             username = 'default'
             if user.is_authenticated:
                 username = user.username
+                await self.create_chat_message(user, msg)
             myResponse = {
                 'message': msg,
                 'username': username
             }
-            await self.send({
-                "type": "websocket.send",
-                "text": json.dumps(myResponse) # Equibalent to JSON.stringify() method
-            })
+
+            # broadcasts the message event to be sent
+            await self.channel_layer.group_send(
+                self.chat_room,
+                {
+                    "type": "chat_message",
+                    "text": json.dumps(myResponse)
+                }
+            )
+
+    async def chat_message(self, event):
+        # sends the actual message
+        await self.send({
+            "type": "websocket.send",
+            "text": event['text']
+        })
 
     async def websocket_disconnect(self, event):
         # When the socket disconnects
@@ -49,3 +72,8 @@ class ChatConsumer(AsyncConsumer):
     @database_sync_to_async  # this is done to prevent memeory leaks,overload the database,open too many connections,etc
     def get_thread(self, user, other_username):
         return Thread.objects.get_or_new(user, other_username)[0]
+
+    @database_sync_to_async
+    def create_chat_message(self, me, msg):
+        thread_obj = self.thread_obj
+        return ChatMessage.objects.create(thread=thread_obj, user=me, message=msg)
